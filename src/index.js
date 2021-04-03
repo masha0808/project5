832de071b6fe447b89cffacbf2861bd812c2ff06
student13 <student@student.com>
2021-04-03 15:14:49 +0300

    add new file

--- src/index.js
+++ src/index.js
@@ -1,1 +1,66 @@
 import './scss/styles.scss'
+
+// {var a = 5;
+// console.log(a);}
+// {let b = 4;
+// console.log(b);}
+
+// console.log(Number(true))
+// console.log(typeof String(1))
+
+// === строгое равенство
+// == с приведением типов
+
+// console.log(1 === true)
+// console.log(1 == true)
+
+// console.log(!1)
+// console.log(!!1)
+// console.log(Boolean(1))
+
+// ! - 'обратное'
+
+// console.log(1 * 'privet')
+
+// let marker = true;
+// if (marker === true) {
+//     console.log("You can write");}  
+//  else
+//     {console.log("You cannot write");
+// } 
+
+// let toChtoVveli = prompt('hey write in me');
+// console.log(typeof +toChtoVveli)
+
+// let age = prompt("How old are you?");
+// if (age > 18 || age == 18) {
+//     console.log("pleasant viewing")
+// }
+// else
+// {
+//     console.log("go and study you homework")
+// }
+
+// and (&&)
+// or (||)
+
+// let gender = "555";
+// switch(gender) {
+//     case("Ukraine"): console.log('Kiev')
+//       break;
+//     case("Italy"): console.log('Rym')
+//       break;
+//     default: console.log('error')
+//       break;
+// }
+
+// let itsNumber = prompt('write number');
+// if(itsNumber > 0) {
+//     console.log(1)
+// }
+// else if(itsNumber == 0){
+//     console.log(0)
+// } 
+// else if(itsNumber < 0){
+//     console.log(-1)
+// }
